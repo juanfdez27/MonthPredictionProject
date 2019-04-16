48cceba7c4d5b72e3cdc75071f309159e929d744
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-04-16 07:44:10 -0400

    Scanner and int introduced

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner keyboard = new Scanner(System.in);
+        int month, year, day;
+        boolean test = true;
+        while (test = true){
+            //System.out.println("Monday's child is fair of face,");
+           // System.out.println("Tuesday's child is full of grace,");
+            //System.out.println("Wednesday's child is full of woe,");
+            //System.out.println("Thursday's child has far to go,");
+            //System.out.println("Friday's child is loving and giving,");
+           // System.out.println("Saturday's child works hard for a living,");
+            //System.out.println("But the child born on the Sabbath day,");
+           // System.out.println("Is fair and wise and good in every way.");
+
+            //System.out.println("What month were you born on?");
+           // month = keyboard.nextInt();
+           // System.out.println("What day were you born on?");
+           // day = keyboard.nextInt();
+           // System.out.println("What year were you born on?");
+           // year = keyboard.nextInt();
+        }
+
+    }
+}
+
+
