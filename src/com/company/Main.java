271bd722b64c41ae8b3c4e6f73458f010bac7022
juan <juan.fernandez.doremire.2020@student.lhschools.org>
2019-04-16 07:44:57 -0400

    Poem staged

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,14 +10,14 @@ public class Main {
         int month, year, day;
         boolean test = true;
         while (test = true){
-            //System.out.println("Monday's child is fair of face,");
-           // System.out.println("Tuesday's child is full of grace,");
-            //System.out.println("Wednesday's child is full of woe,");
-            //System.out.println("Thursday's child has far to go,");
-            //System.out.println("Friday's child is loving and giving,");
-           // System.out.println("Saturday's child works hard for a living,");
-            //System.out.println("But the child born on the Sabbath day,");
-           // System.out.println("Is fair and wise and good in every way.");
+            System.out.println("Monday's child is fair of face,");
+            System.out.println("Tuesday's child is full of grace,");
+            System.out.println("Wednesday's child is full of woe,");
+            System.out.println("Thursday's child has far to go,");
+            System.out.println("Friday's child is loving and giving,");
+            System.out.println("Saturday's child works hard for a living,");
+            System.out.println("But the child born on the Sabbath day,");
+            System.out.println("Is fair and wise and good in every way.");
 
             //System.out.println("What month were you born on?");
            // month = keyboard.nextInt();
